80fa801f97999303d5533fc3742cc0b06a00a3ac
Ahmet Cansever <ahmettcansever@gmail.com>
2021-01-13 14:07:32 +0100

    updated

---
+++ Assignments 1-2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#Python assignment 007/1
+name = input("write your name : ")
+if name == "Ahmet":
+    print("Hello Ahmet! the password is : W@12 ")
+else:
+        print("Hello," + name, "See you latter.")
+
+
+# In[12]:
+
+
+#Python assignment 007/2(Covid-19)
+age = False
+chronic = True
+immune = True
+risk= (age and chronic and immune)
+if risk == True:
+    print(" You are in risky group")
+else:
+        print("You are not in risky group")
+
+
+# In[ ]:
+
+
+
+
