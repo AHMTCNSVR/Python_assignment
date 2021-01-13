a077eaca9b6e9fe514ed6fb967925d0157da46f9
Ahmet Cansever <ahmettcansever@gmail.com>
2021-01-13 13:54:14 +0100

    Assignmet_python

---
+++ assignment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+name = input("write your name : ")
+if name == "Ahmet":
+    print("Hello Ahmet! the password is : W@12 ")
+else:
+        print("Hello," + name, "See you latter.")
+
+
+# In[ ]:
+
+
+
+
